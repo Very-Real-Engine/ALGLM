2dd5d816585cdb92d2ca2b700d54e9e78837c019
seonjo1 <tjdgus9684@gmail.com>
2025-02-08 02:13:30 +0900

    [Feat]: add alglm.h

---
+++ include/alglm.h
@@ -0,0 +1,13 @@
+#ifndef SIMD_MATH_H
+#define SIMD_MATH_H
+
+#include "simd/compiler_config.h"
+#include "simd/platform.h"
+
+#include "math/math_utils.h"
+#include "math/vec3.h"
+#include "math/vec4.h"
+#include "math/mat4.h"
+#include "math/quat.h"
+
+#endif // SIMD_MATH_H
