8f3f23e78133f00a0ca5da77649dc3b26de4d103
seonjo1 <tjdgus9684@gmail.com>
2025-02-08 02:13:50 +0900

    [Feat]: add compiler_config.h

---
+++ include/simd/compiler_config.h
@@ -0,0 +1,26 @@
+#ifndef COMPILER_CONFIG_H
+#define COMPILER_CONFIG_H
+
+// 컴파일러별 인라인 처리
+#ifdef _MSC_VER
+    // MSVC에서는 강제 인라인: __forceinline
+    #define FORCE_INLINE __forceinline
+    // MSVC에서는 alignas 대신 __declspec(align(N)) 사용
+    #define ALIGN16 __declspec(align(16))
+    #define ALIGN32 __declspec(align(32))
+    #define ALIGN64 __declspec(align(64))
+#elif defined(__GNUC__) || defined(__clang__)
+    // GCC, Clang에서는 __attribute__((always_inline))와 inline
+    #define FORCE_INLINE __attribute__((always_inline)) inline
+    // GCC, Clang에서는 __attribute__((aligned(N))) 사용
+    #define ALIGN16 __attribute__((aligned(16)))
+    #define ALIGN32 __attribute__((aligned(32)))
+    #define ALIGN64 __attribute__((aligned(64)))
+#else
+    #define FORCE_INLINE inline
+    #define ALIGN16 alignas(16)
+    #define ALIGN32 alignas(32)
+    #define ALIGN64 alignas(64)
+#endif
+
+#endif // COMPILER_CONFIG_H
