a9dd651128885ca2c1d31840f419e2de195d94a7
seonjo1 <tjdgus9684@gmail.com>
2025-03-26 13:52:03 +0900

    [Fix]: change to scalar

--- include/simd/platform.h
+++ include/simd/platform.h
@@ -4,10 +4,10 @@
 // CPU 아키텍처에 따라 적절한 SIMD 헤더 파일을 포함
 
 // #ifdef __AVX2__
-#include "simd_avx2.h"
+// #include "simd_avx2.h"
 // #else
 //     #include "simd_scalar.h"
 // #endif
-// #include "simd_scalar.h"
+#include "simd_scalar.h"
 
 #endif // PLATFORM_H
